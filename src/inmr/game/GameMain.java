3e0d5c5a8f9bce8156e45245ce67d6079666ae09
game01 <game01@dell2016g011>
2017-10-26 12:29:43 +0900

    sssssssss

--- src/inmr/game/GameMain.java
+++ src/inmr/game/GameMain.java
@@ -1,7 +1,13 @@
 package inmr.game;
 
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Insets;
+import java.awt.image.BufferStrategy;
 import java.util.Timer;
 import java.util.TimerTask;
+
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
 public class GameMain {
@@ -14,17 +20,67 @@ public class GameMain {
 			}
 		});
 	}
-	
+
+	BufferStrategy bs;
+	Insets insets;
+
+	Input input = new Input();
+	JFrame window = new JFrame("STG");
 	Timer timer = new Timer();
-	MainUpdate mainupdate = new MainUpdate();
-	
-	GameMain(){
-		timer.schedule(mainupdate, 17, 17);
+	MainLoop loop = new MainLoop();
+
+	GameGraphics gg = new GameGraphics();
+	GameManager mgr = new GameManager(gg);
+
+	GameMain() {
+
+		window.setIgnoreRepaint(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(false);
+		window.setVisible(true);
+		insets = window.getInsets();
+		int sizeW = GameSetting.WINDOW_W + insets.left + insets.right;
+		int sizeH = GameSetting.WINDOW_H + insets.top + insets.bottom;
+		window.setSize(sizeW, sizeH);
+		window.setLocationRelativeTo(null);
+
+		window.createBufferStrategy(2);
+		bs = window.getBufferStrategy();
+
+		window.addMouseListener(input);
+		window.addMouseMotionListener(input);
+		window.addKeyListener(input);
+		window.addMouseWheelListener(input);
+
+		timer.schedule(loop, 17, 17);
 	}
-	
-	class MainUpdate extends TimerTask {
+
+	class MainLoop extends TimerTask {
 		public void run() {
-			
+
+			input.update(window);
+			mgr.update();
+
+			Graphics g = bs.getDrawGraphics();
+			Graphics2D backLayer = (Graphics2D) g; // Background
+			Graphics2D midLayer = (Graphics2D) g; // Main
+			Graphics2D frontLayer = (Graphics2D) g; // Effects
+			Graphics2D uiLayer = (Graphics2D) g; // UI
+
+			if (bs.contentsLost() == false) {
+				backLayer.translate(insets.left, insets.top);
+				midLayer.translate(insets.left, insets.top);
+				frontLayer.translate(insets.left, insets.top);
+
+				gg.update(backLayer, midLayer, frontLayer, uiLayer, window);
+
+				bs.show();
+				backLayer.dispose();
+				midLayer.dispose();
+				frontLayer.dispose();
+				uiLayer.dispose();
+			}
+
 		}
 	}
 
